a7d4ac8063861184ac9b28fd50ad3b2de1754d7e
Atulkumar899111 <atul.kumar899111@gmail.com>
2024-01-18 16:56:18 +0530

    Added new colour change button

--- firts-react-app/src/main.jsx
+++ firts-react-app/src/main.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css';
-import CompletedList from './CompleteList'
-import Data from './Components/Data'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import CompletedList from "./CompleteList";
+import Data from "./Components/Data";
+import Heding from "./Heding";
+import Form from "./Form";
+import List from "./List";
 
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Heding/>
-    <Form/>
-    <List/> */}
-    {/* <CompletedList/> */}
-    <Data/>
+    {/* <Heding />
+    <Form />
+    <List />
+    <CompletedList /> */}
+    <Data />
   </React.StrictMode>
-)
+);
