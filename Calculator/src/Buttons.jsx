f044779d02ddadf75eadae4cfc4cf2f13f784fbf
Atulkumar899111 <atul.kumar899111@gmail.com>
2024-01-20 14:24:05 +0530

    NB1

---
+++ Calculator/src/Buttons.jsx
@@ -0,0 +1,15 @@
+import "./Buttons.css";
+const Button = ({ buttonName, getButtonValue }) => {
+  return (
+    <>
+      <button>
+        <span className="button_top" onClick={() => getButtonValue(buttonName)}>
+          {" "}
+          {buttonName}
+        </span>
+      </button>
+    </>
+  );
+};
+
+export default Button;
